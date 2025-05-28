d3305d9867ce82527049be43bd9572a510621f8f
z3nZ3r0 <90191007+z3nZ3r0@users.noreply.github.com>
2025-05-28 14:26:31 +0200

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -12,8 +12,22 @@ var event = {
 	title : "",
 	text : "",
 	image : "",
-	link : ""
+	linkText : "",
+	linkUrl : ""
 };
+function eventDisplay (force = false) {
+	if(event.start && event.end || force){
+		let evHTML = `<h2>${event.title}</h2>
+<p>${event.title}</p>
+<a class="button" href="${event.linkUrl}">${event.linkText}</a>
+<img src="${event.image}" />
+`;	
+evSection = document.getElementById("event");
+		evSection.style.display = "block";
+
+	}
+
+}
 
 function UrlCopie() {
 	navigator.clipboard.writeText('fbft.fr')
