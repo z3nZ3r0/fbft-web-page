179668dce656c3aa7c30bde96dcbec232c8a0415
z3nZ3r0 <90191007+z3nZ3r0@users.noreply.github.com>
2025-05-28 20:27:28 +0200

    Add files via upload

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,36 +1,54 @@
-var url = {
-	ytChannel : "",
-	ytVideo : "",
-	discord : "",
-	patreon : ""
-	paypal : ""
+
+const URLs = {
+	ytChannel: "yo",
+	ytVideo: "yo",
+	discord: "yo",
+	patreon: "yo",
+	paypal: "yo"
 };
 
-var event = {
-	start : new Date("2020-1-1"), 
-	end : new Date("2020-1-1"),
-	title : "",
-	text : "",
-	image : "",
-	linkText : "",
-	linkUrl : ""
+var eventSec = {
+	start : new Date("2025-1-1"), 
+	end : new Date("2026-1-1"),
+	title : "titre de l'evenement",
+	text : "corp du texte",
+	image : "#",
+	linkText : "texte du bouton",
+	linkUrl : "#"
 };
+/*
 function eventDisplay (force = false) {
 	let today = new Date();
-	if((event.start <= today && event.end >= today) || force){
-		let evHTML = `<h2>${event.title}</h2>
-<p>${event.title}</p>
-<a class="button" href="${event.linkUrl}">${event.linkText}</a>
-<img src="${event.image}" />
-`;	
-evSection = document.getElementById("event");
+	if((eventSec.start <= today && eventSec.end >= today) || force) {
+		
+		let evHTML = `<h2>${eventSec.title}</h2>
+<p>${eventSec.title}</p>
+<a class="button" href="${eventSec.linkUrl}">${eventSec.linkText}</a>
+<img src="${eventSec.image}" />
+`;
+
+
+		evSection = document.getElementById("event");
 		evSection.style.display = "block";
 		evSection.innerHTML = evHTML;
 	}
 
 }
-
+*/
+function eventDisplay (force = false) {
+	var today = new Date();
+	if((eventSec.start <= today && eventSec.end >= today) || force) {
+			var evHTML = `<h2>${eventSec.title}</h2>
+<p>${eventSec.title}</p>
+<a class="button" href="${eventSec.linkUrl}">${eventSec.linkText}</a>
+<img src="${eventSec.image}" />
+`;
+	}
+	evSection = document.getElementById("event");
+	evSection.style.display = "block";
+	evSection.innerHTML = evHTML;
+}
 function UrlCopie() {
 	navigator.clipboard.writeText('fbft.fr')
-	document.getElementById("url").textContent = "adresse copier !";
+	document.getElementById("url").textContent = "adresse du serveur copier !";
 }
